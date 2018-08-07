4d7359438c42e3f8f0724d853bf4fb5de3e22a82
fabuss254 <fabuss254@outlook.fr>
2018-08-07 16:15:25 +0200

    Update Web.js

--- .gitignore/Web.js
+++ .gitignore/Web.js
@@ -3,10 +3,22 @@ const request = require('request').defaults({ encoding: null });
 const http = require("http");
 var bot = new Discord.Client();
 var ch = "476389923574775823";
+var co = "476391476318830598";
+var owner = "178131193768706048";
 
 
 bot.on("ready", function(){
   bot.channels.get(ch).send("Restarted successfully!")
+});
+
+bot.on("message", function(){
+  if (message.author.equals(bot.user) || message.channel.id != co || message.author.id != owner) return;
+    var args = message.content.split(" ");
+    if (args[0].toLowerCase() == "login"){
+      message.channel.send("Login in...")
+    }else{
+      message.channel.send("Invalid command.")
+    }
 });
 
 bot.login(process.env.TOKEN);
